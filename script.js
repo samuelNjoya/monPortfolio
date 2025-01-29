288fd169af9b61da96b6740373f7678a4fd1b6f7
Njoya samuel <samuelnjoya08@gmail.com>
2025-01-29 17:51:34 +0100

    second commit

--- script.js
+++ script.js
@@ -6,15 +6,40 @@ $(document).ready(function () {
         }else{
             $('.navbar').removeClass("sticky");
         }
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+
     });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+     });
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
+     //animation script
+    var typed = new Typed(".typing", {
+        strings: ["web Developper","Designer","Freelancer","Analyst"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
+    });
+     
+    var typed = new Typed(".typing-2", {
+        strings: ["web Developper","Designer","Freelancer","Analyst"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true
+    });
 
-// owl carousel script
+  // owl carousel script
     $('.carousel').owlCarousel({
         margin:20,
         loop:true,
